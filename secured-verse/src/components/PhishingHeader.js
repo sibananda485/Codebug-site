a5a166f685610e732182aec0d6c33898cf384b2a
sibananda485 <sahusiba485@gmail.com>
2023-12-18 01:30:43 +0530

    Adding Copy functionality

--- secured-verse/src/components/PhishingHeader.js
+++ secured-verse/src/components/PhishingHeader.js
@@ -1,13 +1,22 @@
 import React from "react";
-import FishingLogo from "../assets/phishing.png";
+import FishingLogo from "../assets/phising/phishing.png";
 import Logo from "../assets/Logo.png";
-import Step1 from "../assets/step1.png";
-import Step2 from "../assets/step2.png";
-import Step3 from "../assets/step3.png";
-import Step4 from "../assets/step4.png";
-import Step5 from "../assets/step5.png";
+import Step1 from "../assets/phising/step1.png";
+import Step2 from "../assets/phising/step2.png";
+import Step3 from "../assets/phising/step3.png";
+import Step4 from "../assets/phising/step4.png";
+import Step5 from "../assets/phising/step5.png";
 
 export default function PhishingHeader() {
+  const handleCopy = (e) => {
+    navigator.clipboard.writeText(
+      e.target.parentElement.parentElement.nextElementSibling.innerText
+    );
+    e.target.previousSibling.classList.remove("hidden");
+    setTimeout(() => {
+      e.target.previousSibling.classList.add("hidden");
+    }, 2000);
+  };
   return (
     <>
       <div className="">
@@ -43,7 +52,7 @@ export default function PhishingHeader() {
               </li>
             </ul>
             <img src={FishingLogo} alt="fishing" className="w-40 sm:w-72" />
-            <h1 className="font-bai text-3xl md:text-5xl">Phishing</h1>
+            <h1 className="font-juli text-3xl md:text-5xl">Phishing</h1>
             <p className="font-advent text-lg text-center text-[rgba(255,255,255,0.68)] md:text-2xl md:text-start">
               Phishing is like a trick played by cybercriminals. They pretend to
               be someone you trust, like a bank or a friend, and try to get your
@@ -53,7 +62,7 @@ export default function PhishingHeader() {
               to keep your information safe.
             </p>
           </div>
-          
+
           {/* Disclaimer */}
           <div className="text-white space-y-5 px-2 my-10 lg:my-10 lg:px-48 ">
             <h1 className="font-handjet text-red-500 text-3xl md:text-5xl text-center">
@@ -72,85 +81,103 @@ export default function PhishingHeader() {
 
           {/* Step - 1 */}
           <div className="bg-gradient-to-r from-black to-[#003706] text-white flex flex-col justify-center lg:shadow-2xl shadow-black gap-5 px-2 sm:px-10 md:px-16 pt-10 lg:my-10 lg:px-48">
-            <h1 className="font-bai text-3xl md:text-5xl">STEP - 1</h1>
-            <h2 className="uppercase text-sm lg:text-2xl">
+            <h1 className="font-juli text-3xl md:text-5xl">STEP - 1</h1>
+            <h2 className="uppercase font-juli text-sm lg:text-2xl">
               To install the tool first go to the desktop directory and then
               install the tool using the following commands.
             </h2>
             <div className="border rounded border-gray-500">
               <div className="bg-gray-700 px-2 flex justify-between items-center py-1">
                 terminal
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                  />
-                </svg>
+                <div className="flex items-center gap-1">
+                  <p className="hidden border border-cyan-500 text-cyan-500 px-2 rounded-md">
+                    Copied
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 cursor-pointer hover:text-cyan-400 transition-all"
+                    onClick={handleCopy}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                    />
+                  </svg>
+                </div>
               </div>
 
-              <p className="px-2 py-1">
+              <p className="px-2 py-1 font-ibm">
                 git clone https://github.com/htr-tech/zphisher.git
               </p>
             </div>
             <div className="border rounded border-gray-500">
               <div className="bg-gray-700 px-2 flex justify-between items-center py-1">
                 terminal
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                  />
-                </svg>
+                <div className="flex items-center gap-1">
+                  <p className="hidden border border-cyan-500 text-cyan-500 px-2 rounded-md">
+                    Copied
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 cursor-pointer hover:text-cyan-400 transition-all"
+                    onClick={handleCopy}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                    />
+                  </svg>
+                </div>
               </div>
 
-              <p className="px-2 py-1">cd zphisher</p>
+              <p className="px-2 py-1 font-ibm">cd zphisher</p>
             </div>
             <img src={Step1} alt="terminal" className="my-5 lg:max-w-2xl" />
           </div>
 
           {/* Step - 2 */}
           <div className="bg-gradient-to-r from-black to-[#003706] text-white flex flex-col justify-center lg:shadow-2xl shadow-black gap-5 px-2  pt-10 lg:my-10 lg:px-48">
-            <h1 className="font-bai text-3xl md:text-5xl">STEP - 2</h1>
-            <h2 className="uppercase text-sm lg:text-2xl">
+            <h1 className="font-juli text-3xl md:text-5xl">STEP - 2</h1>
+            <h2 className="uppercase font-juli text-sm lg:text-2xl">
               Now you are in zphisher directory , use the following command to
               run the tool.
             </h2>
             <div className="border rounded border-gray-500">
               <div className="bg-gray-700 px-2 flex justify-between items-center py-1">
                 terminal
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                  />
-                </svg>
+                <div className="flex items-center gap-1">
+                  <p className="hidden border border-cyan-500 text-cyan-500 px-2 rounded-md">
+                    Copied
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 cursor-pointer hover:text-cyan-400 transition-all"
+                    onClick={handleCopy}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                    />
+                  </svg>
+                </div>
               </div>
 
-              <p className="px-2 py-1">bash zphisher.sh</p>
+              <p className="px-2 py-1 font-ibm">bash zphisher.sh</p>
             </div>
 
             <img src={Step2} alt="terminal" className="my-5 lg:max-w-2xl" />
@@ -158,8 +185,8 @@ export default function PhishingHeader() {
 
           {/* Step - 3 */}
           <div className="bg-gradient-to-r from-black to-[#003706] text-white flex flex-col justify-center lg:shadow-2xl shadow-black gap-5 px-2  pt-10 lg:my-10 lg:px-48">
-            <h1 className="font-bai text-3xl md:text-5xl">STEP - 3</h1>
-            <h2 className="uppercase text-sm lg:text-2xl">
+            <h1 className="font-juli text-3xl md:text-5xl">STEP - 3</h1>
+            <h2 className="uppercase font-juli text-sm lg:text-2xl">
               The tool has started running successfully. Now you have to choose
               the options from the tool for which you have to make the phishing
               page.
@@ -169,8 +196,8 @@ export default function PhishingHeader() {
 
           {/* Step - 4 */}
           <div className="bg-gradient-to-r from-black to-[#003706] text-white flex flex-col justify-center lg:shadow-2xl shadow-black gap-5 px-2  pt-10 lg:my-10 lg:px-48">
-            <h1 className="font-bai text-3xl md:text-5xl">STEP - 4</h1>
-            <h2 className="uppercase text-sm lg:text-2xl">
+            <h1 className="font-juli text-3xl md:text-5xl">STEP - 4</h1>
+            <h2 className="uppercase font-juli text-sm lg:text-2xl">
               From these options, you can choose the number for which you have
               to create a phishing page. Suppose you want to create a phishing
               page for Instagram then choose option 2.{" "}
@@ -180,8 +207,8 @@ export default function PhishingHeader() {
 
           {/* Step - 5 */}
           <div className="bg-gradient-to-r from-black to-[#003706] text-white flex flex-col justify-center lg:shadow-2xl shadow-black gap-5 px-2  pt-10 lg:my-10 lg:px-48">
-            <h1 className="font-bai text-3xl md:text-5xl">STEP - 5</h1>
-            <h2 className="uppercase text-sm lg:text-2xl">
+            <h1 className="font-juli text-3xl md:text-5xl">STEP - 5</h1>
+            <h2 className="uppercase font-juli text-sm lg:text-2xl">
               Now you can see that to attract the victim , it’s giving 4
               different web templates. You can choose any option from here.
               Suppose you want to choose the first option then type 1..
