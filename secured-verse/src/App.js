9e18e538fb7e866852f6325e9eb68b8384e9deeb
Sibananda sahu <115389688+sibananda485@users.noreply.github.com>
2023-12-21 01:17:47 +0530

    Spelling correction in router

--- secured-verse/src/App.js
+++ secured-verse/src/App.js
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
     element: <ScriptMenu></ScriptMenu>,
   },
   {
-    path: "fishingchecker",
+    path: "phishingchecker",
     element: <FishingChecker></FishingChecker>,
   },
   {
